8b4e26232b484dacdcc89948f66fd28822a207a4
sohamPatkar <spatkar398@gmail.com>
2025-08-29 23:33:18 +0530

    Changed Reset Hinge and Navigation Joystick

--- Assets/Scripts/Interactions/NavmeshJoystick.cs
+++ Assets/Scripts/Interactions/NavmeshJoystick.cs
@@ -7,9 +7,28 @@ public class NavmeshJoystick : SimpleHingeInteractable
     [SerializeField] private Transform trackedObject;
     [SerializeField] private RobotNavigation robotNavigation;
 
+    private Transform startPos;
+    private float startPosX;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        startPos = transform;
+
+        startPosX = startPos.localEulerAngles.x;
+
+        if (startPosX >= 180)
+        {
+            startPosX -= 360;
+        }
+    }
+
     protected override void ResetHinge()
     {
+        Debug.Log(startPosX);
 
+        transform.localEulerAngles = new Vector3(startPosX, 0, 0);
     }
 
     protected override void Update()
